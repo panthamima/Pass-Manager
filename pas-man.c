fd88ec781f3852d23823558b1dd0942259e6f41d
panthamima <nastyadima2003@mail.ru>
2022-01-06 21:46:58 +0700

    PM v0.1

--- pas-man.c
+++ pas-man.c
@@ -1,18 +1,67 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <ncurses.h>
-// #include <stdlib.h>
-// #include "pas-man.h"
 
-// typedef struct pass_storage {
-//     char corp[30], account[50], password[25];
-//     // int id;
-// } storage;
-int main(int num) {
-    char a[100];
-    
-    getch();
-        while(1) {
-            getch();
-        }
 
+// int main() {
+//     unsigned int key = 0;
+//     char corp[100];
+//     char pass[100];
+
+//     FILE *pm;
+//     char name[] = "p.txt";
+
+//     if(pm = fopen(name, "a")) {
+//         printf("enter corp:pass\n");
+//         fscanf(pm, "%d %s:%s", key, corp, pass);
+//         fclose(pm);
+//     }
+// }
+
+// int main()
+// {
+//   FILE *pm;
+//   unsigned int key = 0;
+//   char* corp = "aaa";
+//   char* pass;
+
+//   system("clear");
+
+//   printf("corp: ");
+//   fputs(corp, pm);
+//   fclose(pm);
+
+//   return 0;
+// }
+
+int main(void)
+{
+    char * filename = "p.txt";
+    char cc[256];
+    FILE *fp;
+     
+    // запись в файл
+    if((fp= fopen(filename, "w"))==NULL) {
+        perror("Error occured while opening file");
+        return 1;
+    }
+    // записываем строку
+    char message = getchar();
+    fputs(message, fp);
+ 
+    fclose(fp);
+     
+    // чтение из файла
+    if((fp= fopen(filename, "r"))==NULL) {
+        perror("Error occured while opening file");
+        return 1;
+    }
+    // пока не дойдем до конца, считываем по 256 байт
+    while((fgets(cc, 256, fp))!=NULL) {
+        printf("%s", cc);
+    }
+     
+    fclose(fp);
+    return 0;
 }
+

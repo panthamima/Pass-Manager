625070b9de1b6d886b9f6adacb936d2bf552fbec
panthamima <nastyadima2003@mail.ru>
2022-01-06 23:46:42 +0700

    PM v0.2

--- pas-man.c
+++ pas-man.c
@@ -2,66 +2,34 @@
 #include <stdio.h>
 #include <ncurses.h>
 
-
-// int main() {
-//     unsigned int key = 0;
-//     char corp[100];
-//     char pass[100];
-
-//     FILE *pm;
-//     char name[] = "p.txt";
-
-//     if(pm = fopen(name, "a")) {
-//         printf("enter corp:pass\n");
-//         fscanf(pm, "%d %s:%s", key, corp, pass);
-//         fclose(pm);
-//     }
-// }
-
-// int main()
-// {
-//   FILE *pm;
-//   unsigned int key = 0;
-//   char* corp = "aaa";
-//   char* pass;
-
-//   system("clear");
-
-//   printf("corp: ");
-//   fputs(corp, pm);
-//   fclose(pm);
-
-//   return 0;
-// }
-
 int main(void)
 {
-    char * filename = "p.txt";
-    char cc[256];
-    FILE *fp;
-     
-    // запись в файл
-    if((fp= fopen(filename, "w"))==NULL) {
-        perror("Error occured while opening file");
-        return 1;
+    FILE *pm;
+    char corp[40];
+    char pass[40];
+    char message[222];
+    
+    
+
+    char filename[] = "p.txt";
+    if((pm = fopen(filename, "a")) == NULL) {
+        printf("error");
     }
-    // записываем строку
-    char message = getchar();
-    fputs(message, fp);
- 
-    fclose(fp);
-     
-    // чтение из файла
-    if((fp= fopen(filename, "r"))==NULL) {
-        perror("Error occured while opening file");
-        return 1;
+
+    int command;
+    printf("enter command: ");
+    scanf("%d", &command);
+    if(command == 1) {
+        printf("enter corp name: ");
+        fgets(corp, 39, stdin);
+        fputs(corp, pm);
+        fclose(pm);
     }
-    // пока не дойдем до конца, считываем по 256 байт
-    while((fgets(cc, 256, fp))!=NULL) {
-        printf("%s", cc);
-    }
-     
-    fclose(fp);
+    unsigned int key = 0;
+    // fgets(message, 221, stdin);
+    // fputs(message, pm);
+    // fclose(pm);
+
     return 0;
 }
 

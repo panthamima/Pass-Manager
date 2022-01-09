3348041ac34780b047932dc8ee8caa34ac206620
panthamima <nastyadima2003@mail.ru>
2022-01-09 18:58:04 +0700

    pm v0.2.1

--- pas-man.c
+++ pas-man.c
@@ -4,32 +4,51 @@
 
 int main(void)
 {
-    FILE *pm;
-    char corp[40];
-    char pass[40];
-    char message[222];
-    
-    
+    FILE *pm; // pass-manager
+    int id = 0; 
+    char corp[255];
+    char pass[255];
 
     char filename[] = "p.txt";
     if((pm = fopen(filename, "a")) == NULL) {
         printf("error");
     }
+    
+    fgets(corp,100 ,stdin);
+    fputs(corp, pm);
 
-    int command;
-    printf("enter command: ");
-    scanf("%d", &command);
-    if(command == 1) {
-        printf("enter corp name: ");
-        fgets(corp, 39, stdin);
-        fputs(corp, pm);
-        fclose(pm);
-    }
-    unsigned int key = 0;
+    fgets(pass,100 ,stdin);
+    fputs(pass, pm);
+
+
+    /*
+        fegts[s] [s] [s]
+            where(storage s s s )
+            fputs(id:s:s)
+    */
+
+    // char filename[] = "p.txt";
+    // if((pm = fopen(filename, "a")) == NULL) {
+    //     printf("error");
+    // }
+
+    // char command[10];
+
+    // system("clear");
+    // printf("enter command: ");
+    // scanf("%c", &command);
+    // if(command == "r") {
+    //     // pm = fopen(filename, "a");
+    //     printf("enter corp name: ");
+    //     fgets(corp, 39, stdin);
+    //     fputs(corp, pm);
+    //     fclose(pm);
+    // }
+    // unsigned int key = 0;
     // fgets(message, 221, stdin);
     // fputs(message, pm);
     // fclose(pm);
 
-    return 0;
+    // return 0;
 }
 

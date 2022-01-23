cd2bf3da1c85dd4f59dd98e5464c69cdc33cd384
equitans <equitans@protonmail.com>
2022-01-23 18:18:25 +0800

    create func (create_person)

--- cli.c
+++ cli.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <locale.h>
 
-//start
-int main(){
+
+int create_person();
+
+int main(int argc,char **argv){
+	//setlocale(LC_ALL, "");
+	if (!strcmp(argv[1], "create")) {
+		if (!strcmp(argv[2], "person")){
+			create_person();
+		}
+	}
+		
+	return 0;
+}
+
+int create_person(){
+	char login[255], passwd[255];
+
+	printf("Create a new person\n\n");
+
+	printf("Enter login\n\t- ");
+	scanf("%255s", login);
+	
+	printf("Enter password\n\t- ");
+	scanf("%255s", passwd);
+	
+	printf("New person:\n\t login: %s\n\t password: %s", login, passwd);
 
 	return 0;
 }
+

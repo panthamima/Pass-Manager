fe41fbbf0f9f146cac7e0b41edc0cb83262b54d3
equitans <equitans@protonmail.com>
2022-01-24 20:21:07 +0800

    save person in .txt file

--- cli.c
+++ cli.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 #include <locale.h>
 
-
 int create_person();
 
 int main(int argc,char **argv){
-	//setlocale(LC_ALL, "");
+	//setlocale(LC_ALL, ""); 
+	
 	if (!strcmp(argv[1], "create")) {
 		if (!strcmp(argv[2], "person")){
 			create_person();
@@ -19,7 +19,8 @@ int main(int argc,char **argv){
 
 int create_person(){
 	char login[255], passwd[255];
-
+	FILE *fp;
+	char name[] = "person.pm";
 	printf("Create a new person\n\n");
 
 	printf("Enter login\n\t- ");
@@ -28,6 +29,15 @@ int create_person(){
 	printf("Enter password\n\t- ");
 	scanf("%255s", passwd);
 	
+	if ((fp = fopen(name, "w+")) == NULL){
+		printf("Error: File open");
+		return 0;
+	}
+
+	fputs(login, fp);
+	
+	fclose(fp);
+
 	printf("New person:\n\t login: %s\n\t password: %s", login, passwd);
 
 	return 0;

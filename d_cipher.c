cae891957587b62261e2911ebaaedce7c098e00e
panthamima <nastyadima2003@mail.ru>
2022-02-03 13:44:09 +0700

    fix encrypt

--- d_cipher.c
+++ d_cipher.c
@@ -3,101 +3,56 @@
 
 #define SIZE 255
 
-// int xor(char xor_str[255], char *xor_key);
-
-// int encrypt(char xorplus_str[255], char xorplus_key[255]) {
-//     char buf;
-//     int i;
-
-
-//     for(i = 0; xorplus_str[i] != '\0'; ++i){
-//         buf = xorplus_str[i];
-//         if(buf >= '!' && buf <= '~'){
-//             buf += strlen(xorplus_key);
-//             if(buf > '~'){
-//                 buf = buf - '~' + '!' - 1;
-//             }
-//             xorplus_str[i] = buf;
-//         }
-//     }
-//     printf("\nEncrypted message before: %s\n", xorplus_str);
-
-//     xor(xorplus_str, xorplus_key); 
-
-//     printf("\nEncrypted message after: %s\n", xorplus_str);
-//     return 0;
-// }
-
-// int decrypt(char xorplus_str[], char *xorplus_key) {
-//     char buf;
-//     int i;
-//     xor(xorplus_str, xorplus_key);
-//     printf("Decrypted message: %s\n", xorplus_str);
-
-//     for(i = 0; xorplus_str[i] != '\0'; ++i){
-//         buf = xorplus_str[i];
-//         if(buf >= '!' && buf <= '~'){
-//             buf -= strlen(xorplus_key);
-// 			if(buf < '!') {
-// 				buf = buf + ('~' - '!') + 1;
-// 			}
-//             xorplus_str[i] = buf;
-//         }
-//     }
-//     printf("Decrypted message: %s\n", xorplus_str);
-//     return 0;
-// }
-int caeXorEnc(char xor_str[255], char *xor_key) {
+int caeXorEnc(char xor_str[SIZE], char *xor_key) {
     int i, k, l, j;
-    i = k = l = j =0;
+    i = k = l = j = 0;
     char buf;
-    char count[255];
-    char output[255];
+    char count[SIZE];
+    char output[SIZE];
     for(i = 0; xor_str[i] != '\0'; ++i){
         buf = xor_str[i];
         if(buf >= '!' && buf <= '~'){
             buf += strlen(xor_key);
-            if(buf > '~'){
-                buf = buf - '~' + '!' - 1;
+            if(buf > '}'){
+                buf = buf - '}' + '!' - 1;
             }
         }
         count[j] = buf;
-        output[l] = count[j++] ^ xor_key[k % strlen(xor_key)];
+        count[j] = count[j++] ^ xor_key[k % strlen(xor_key)];
+        printf("%c|", count[j]);
         k++;
-        printf("%c", output[l++]);
     }
 }
 
-int caeXorDec(char xor_str[255], char *xor_key) {
+int caeXorDec(char xor_str[SIZE], char *xor_key) {
     char buf;
-    int i, k, l;
-    i = k = l =0;
-    char output[255];
+    char count[SIZE];
+    int i, k, l, j;
+    i = k = l = j =0;
+    char output[SIZE];
 
     for(i = 0; xor_str[i] != '\0'; ++i){
         output[l] = xor_str[i] ^ xor_key[k % strlen(xor_key)];
+        i++;
         k++;
         buf = output[l];
-        printf("%c", output[l]);
-
         if(buf >= '!' && buf <= '~'){
             buf -= strlen(xor_key);
 			if(buf < '!') {
-				buf = buf + ('~' - '!') + 1;
+				buf = buf + ('}' - '!') + 1;
 			}
-            xor_str[i] = buf;
         }
-        printf("%c|", xor_str[i]);
+        printf("%c|", buf);
     }
 }
 
 int main() {
-    char message[255];
-    char key[] = "1123123312";
+    char message[SIZE];
+    char key[] = "aa33dss";
 
     scanf("%s", message);
 
-    caeXorEnc(message, key);
+    caeXorDec(message, key);
     
 
     return 0;

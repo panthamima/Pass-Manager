6addef8674d308adea3d4f1ac5df77595ab6c3f5
panthamima <nastyadima2003@mail.ru>
2022-02-04 21:17:28 +0700

    finished enc+dec+need refactor

--- d_cipher.c
+++ d_cipher.c
@@ -1,59 +1,63 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define SIZE 255
 
+FILE *AWE;
+char fn[] = "base.db";
+
 int caeXorEnc(char xor_str[SIZE], char *xor_key) {
-    int i, k, l, j;
-    i = k = l = j = 0;
-    char buf;
+    int i, // xor_str
+        j, // count 
+        k; // k % strlen
+
+    i = k = j = 0;
+    char buf; 
     char count[SIZE];
-    char output[SIZE];
+    AWE = fopen(fn, "a");
+
     for(i = 0; xor_str[i] != '\0'; ++i){
         buf = xor_str[i];
-        if(buf >= '!' && buf <= '~'){
-            buf += strlen(xor_key);
+        if(buf >= '!' && buf <= '}'){ // от минимально до максимально доступного чара
+            buf += strlen(xor_key); // чар + сдвиг
             if(buf > '}'){
                 buf = buf - '}' + '!' - 1;
             }
         }
         count[j] = buf;
-        count[j] = count[j++] ^ xor_key[k % strlen(xor_key)];
-        printf("%c|", count[j]);
+        count[j] = count[j] ^ xor_key[k % strlen(xor_key)]; // xor
+        fputc(count[j], AWE);
         k++;
     }
 }
 
 int caeXorDec(char xor_str[SIZE], char *xor_key) {
     char buf;
-    char count[SIZE];
-    int i, k, l, j;
-    i = k = l = j =0;
+    int i, k, j;
+    i = k = j =0;
     char output[SIZE];
-
-    for(i = 0; xor_str[i] != '\0'; ++i){
-        output[l] = xor_str[i] ^ xor_key[k % strlen(xor_key)];
-        i++;
+    AWE = fopen(fn, "a");
+    for(i = 0; xor_str[i] != '\0'; i++){
+        output[j] = xor_str[i] ^ xor_key[k % strlen(xor_key)]; //xor
         k++;
-        buf = output[l];
+        buf = output[j];
         if(buf >= '!' && buf <= '~'){
             buf -= strlen(xor_key);
 			if(buf < '!') {
-				buf = buf + ('}' - '!') + 1;
+				buf = buf + '}' - '!' + 1;
 			}
         }
-        printf("%c|", buf);
+        fputc(buf, AWE);
     }
 }
 
 int main() {
     char message[SIZE];
-    char key[] = "aa33dss";
+    char key[] = "3478fh"; //aa33dss sdfgkhjgh4khjkjjk
 
     scanf("%s", message);
-
     caeXorDec(message, key);
-    
 
     return 0;
 }

c025ddbcafb5055605ef50b8b10a3f8847e386b9
panthamima <nastyadima2003@mail.ru>
2022-02-06 15:33:12 +0700

    createpass function+

--- cli.c
+++ cli.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define SIZE 255
 
-void prepareString(); 	// создание строки для записи в файл
-void addition();		// добавление строки в файл
-int  counting();        // подсчет строк
-void extradition();     // выдача информации о записи (строке)
-void logging();         // создание мастер аккаунта
-void removing();        // удаление записи из файла
-void showTheList();     // показать все пароли из файла
-void shred();           // удалить все пароли
+void prepareString(); 	    // создание строки для записи в файл
+void addition();		    // добавление строки в файл
+int  counting();            // подсчет строк
+void extradition();         // выдача информации о записи (строке)
+void logging();             // создание мастер аккаунта
+void removing();            // удаление записи из файла
+void showTheList();         // показать все пароли из файла
+void shred();               // удалить все пароли
 void iToc(int n, char s[]); // int to char
-void reverse(char s[]); // reverse string
-
+void reverse(char s[]);     // reverse string
+int createPass();           // создание случайного пароля
 
 FILE *AWE;
 char fn[] = "base.db";
@@ -39,6 +40,9 @@ int main(int argc,char **argv){
     else if(!strcmp(argv[1], "shw")) {
         showTheList();
     }
+    else if(!strcmp(argv[1], "cre")) {
+        createPass();
+    }
     else {
         printf("<ERROR> enter command like: awestruck reg|add|rem|get\n");
     }
@@ -49,7 +53,8 @@ void logging() {
     addition();
 }
 
-void reverse(char s[]) { //reverse string
+//reverse string
+void reverse(char s[]) { 
     int i, j;
     char c;
     
@@ -63,17 +68,20 @@ void reverse(char s[]) { //reverse string
 // int to char
 void iToc(int n, char s[]) { 
     int i, sign;
+    i = 0;
 
     if ((sign = n) < 0) { /* записываем знак */
         n = -n;          /* делаем n положительным числом */
     } 
-    i = 0;
+    
     do {       /* генерируем цифры в обратном порядке */
         s[i++] = n % 10 + '0';   /* берем следующую цифру */
     } while ((n /= 10) > 0);     /* удаляем */
+
     if (sign < 0) {
         s[i++] = '-';
     }
+
     s[i] = '\0';
     reverse(s);
 }
@@ -122,7 +130,7 @@ void prepareString() {
 }
 
 // добавление строки в файл
-void addition() { 
+void addition() { // если нажать пробел запись багается
     AWE = fopen(fn, "a");
     
     counting();
@@ -143,12 +151,27 @@ void shred() {
 
 }
 
-
 void removing() {
 
 }
 
+int createPass() {
+    int i;
+    int passLen = 24;
+    char randPass[23];
+    char randCh;
+    char pool[] = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
 
-void extradition() { // выдача информации о записи (строке)
+    srand(time(NULL));
+
+    for(i = 0; i < passLen; i++) {
+        randCh = rand()%62;
+        randPass[i] = pool[randCh];
+    }
+    // printf("%s\n", randPass);
+}
+
+// выдача информации о записи (строке)
+void extradition() { 
     printf("");
 }
